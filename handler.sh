16db1466d63b993edd913a4bf21f3fb97a9ad5c0
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-08-17 22:23:26 +0200

    improve config and restart waybar only when theme change

--- handler.sh
+++ handler.sh
@@ -7,21 +7,26 @@ fi
 
 dir="$1"
 theme="$2"
-dark="$3"
+gtk_theme="$3"
 img="$4"
 echo dir ${dir}
 echo theme ${theme}
-echo dark ${dark}
+echo gtk_theme ${gtk_theme}
 echo img ${img}
 
-cp ${dir}/base.css ~/.config/waybar/style.css
-cat ${dir}/styles/${theme}.css >> ~/.config/waybar/style.css
-
-xfconf-query -c xsettings -p /Net/ThemeName -s "Adwaita${dark}"
-xfconf-query -c xsettings -p /Net/IconThemeName -s "Adwaita${dark}"
-
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita${dark}"
-gsettings set org.gnome.desktop.interface icon-theme "Adwaita${dark}"
-
 swww img ${dir}/wallpapers/${img} --transition-fps 60 --transition-type grow --transition-pos 1.0,1.0 --transition-duration 0.5
-killall -SIGUSR2 waybar
+
+if [[ $theme != "skipvalue" ]]; then
+    cp ${dir}/base.css ~/.config/waybar/style.css
+    if [[ $theme != "" ]]; then
+        cat ${dir}/styles/${theme}.css >> ~/.config/waybar/style.css
+    else
+        echo "just reset"
+    fi
+    killall -SIGUSR2 waybar
+fi
+
+if [[ $gtk_theme != "skipvalue" ]]; then
+    xfconf-query -c xsettings -p /Net/ThemeName -s "${gtk_theme}"
+    gsettings set org.gnome.desktop.interface gtk-theme "${gtk_theme}"
+fi
--- theme_switch.py
+++ theme_switch.py
@@ -3,13 +3,14 @@ import subprocess, os, sys, json
 def run_command(command):
     subprocess.Popen(command,shell=True)
 
-def load_theme(theme):
-    if "theme" in theme:
-        theme_override = theme["theme"]
-    else:
-        theme_override = "dark" if theme["dark"] else "light"
-    dark = "-dark" if theme["dark"] else '""'
-    run_command(f"{handler_file_path} {config_path} {theme_override} {dark} {theme['img']}")
+def load_theme(theme, old_theme):
+    css_theme = "skipvalue"
+    if old_theme.get("theme","skipvalue") != theme.get("theme","skipvalue"):
+        css_theme = theme.get("theme",'""')
+    gtk_theme = "skipvalue"
+    if old_theme.get("dark",True) != theme.get("dark",True):
+        gtk_theme = theme.get("dark",True)
+    run_command(f"{handler_file_path} {config_path} {css_theme} {gtk_theme} {theme['img']}")
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 handler_file_path = os.path.join(cwd,"handler.sh")
@@ -31,14 +32,27 @@ if not os.path.isdir(config_path):
         f.write("")
     with open(config_file_path,"w") as f:
         json.dump({
+            "gtk_theme_light":"Adwaita",    # gtk theme when "dark" is True
+            "gtk_theme_dark":"Adwaita-dark",# gtk theme when "dark" is False
+
             "themes":[
                 {
-                    "img":"dark.png",
-                    "dark":True
+                    "img":"light.png", # wallpaper
+                    "dark":False # if to use dark gtk theme
                 },
                 {
-                    "img":"light.png",
-                    "dark":False
+                    "img":"dark.png"
+                    # no need to specify dark, is default
+                    # you can however (and most likely want to) specify
+                    # a dark override theme
+                    # ex: "theme":"my_dark_override"
+                    # will append the contents of ~/.config/styles/my_dark_override.css to
+                    # base.css, allowing you to override the default theme
+                },
+                {
+                    "img":"bright yellow.png",
+                    "dark":False, # use light gtk theme
+                    "theme":"yellow" # override default theme
                 }
             ]
         },f,indent=4)
@@ -51,11 +65,13 @@ with open(config_file_path, "r") as f:
 
 themes = config["themes"]
 
+old_theme = themes[current]
+
 if len(sys.argv) > 1 and sys.argv[1] == "previous":
     current -= 1
 elif len(sys.argv) > 1 and sys.argv[1] == "next":
     current += 1
-    
+
 if current >= len(themes):
     current = 0
 if current < 0:
@@ -65,5 +81,4 @@ theme = themes[current]
 with open(current_file_path,"w") as f:
     f.write(str(current))
 
-load_theme(theme)
-
+load_theme(theme,old_theme)
